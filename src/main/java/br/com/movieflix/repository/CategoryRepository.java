a526634540712de7ac46598eec1f8633e7552ad1
adaurideveloper-sudo <adauri.developer@gmail.com>
2026-03-14 16:37:40 -0300

    feat: adiciona a interface CategoryRepository na aplicação

--- src/main/java/br/com/movieflix/repository/CategoryRepository.java
+++ src/main/java/br/com/movieflix/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package br.com.movieflix.repository;
 
-public interface CategoryRepository {
+import br.com.movieflix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
