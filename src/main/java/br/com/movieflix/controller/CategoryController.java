31c1fc693cb0970735b565168d90f24150640b29
adaurideveloper-sudo <adauri.developer@gmail.com>
2026-03-14 16:43:14 -0300

    feat: adiciona a rota post no Controller e o metodo saveCategory no Service

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -1,4 +1,31 @@
 package br.com.movieflix.controller;
 
+import br.com.movieflix.entity.Category;
+import br.com.movieflix.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/movieflix/category")
+@RequiredArgsConstructor
 public class CategoryController {
+
+    private  final CategoryService categoryService;
+
+    @GetMapping()
+    public List<Category> getAllCategories() {
+        return categoryService.findAll();
+    }
+
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
 }
--- src/main/java/br/com/movieflix/service/CategoryService.java
+++ src/main/java/br/com/movieflix/service/CategoryService.java
@@ -1,4 +1,24 @@
 package br.com.movieflix.service;
 
+import br.com.movieflix.entity.Category;
+import br.com.movieflix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+    public Category saveCategory(@RequestBody Category category) {
+        return repository.save(category);
+    }
 }
