15670e76c9c23646d4b7a2b67011be9542509b2d
adaurideveloper-sudo <adauri.developer@gmail.com>
2026-03-14 16:40:43 -0300

    fix: adiciona a annotation NotBlank na string name

---
+++ src/main/java/br/com/movieflix/entity/Category.java
@@ -0,0 +1,27 @@
+package br.com.movieflix.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    @NotBlank(message = "O nome da categoria é obrigatória")
+    private String name;
+
+}
