f7e365fb29110b7eb0cc00d8d0bf43bdff09150c
adaurideveloper-sudo <adauri.developer@gmail.com>
2026-03-16 16:22:47 -0300

    feat: cria as rotas de procurar categoria por id e deletar categorias por id no controller

--- src/main/java/br/com/movieflix/controller/CategoryController.java
+++ src/main/java/br/com/movieflix/controller/CategoryController.java
@@ -3,13 +3,16 @@ package br.com.movieflix.controller;
 import br.com.movieflix.entity.Category;
 import br.com.movieflix.service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController()
 @RequestMapping("/movieflix/category")
@@ -26,6 +29,19 @@ public class CategoryController {
     @PostMapping
     public Category saveCategory(@RequestBody Category category) {
         return categoryService.saveCategory(category);
+    }
+
+    @GetMapping("/{id}")
+    public  Category getByCategoryId(@PathVariable Long id) {
+        Optional<Category> optionalCategory = categoryService.findById(id);
+        if (optionalCategory.isPresent()) {
+            return optionalCategory.get();
+        }
+        return null;
+    }
+    @DeleteMapping("/{id}")
+    public void deleteByCategoryId(@PathVariable Long id) {
+        categoryService.deleteByCategoryId(id);
     }
 
 }
